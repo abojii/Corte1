2cd6a49bfd8a09436a25857d7c55b2858f8db1b8
abojii <abojorgec@gmail.com>
2024-09-23 10:04:03 -0600

    Obetenemos la Clase registro

---
+++ Sistematico/Ejercicio1/Persona/persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Persona
+{
+    internal class persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string fechadenacimiento { get; set; }
+        public string ciudad { get; set; }
+    }
+}
---
+++ Sistematico/Ejercicio1/Registro/registro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1.Registro
+{
+    internal class registro
+    {
+        private string[] nombres = new string[30];
+        private string[] apellidos = new string[30];
+        private DateTime[] fechasNacimiento = new DateTime[30];
+        private string[] ciudades = new string[30];
+        private int contador = 0;
+
+        public void AgregarPersona(string nombre, string apellido, DateTime fechaNacimiento, string ciudad)
+        {
+            if (contador < 30)
+            {
+                nombres[contador] = nombre;
+                apellidos[contador] = apellido;
+                fechasNacimiento[contador] = fechaNacimiento;
+                ciudades[contador] = ciudad;
+                contador++;
+
+            }
+            else
+            {
+                MessageBox.Show("No puede agregar más personas");
+            }
+        }
+
+        public string[] GetNombres()
+        {
+            return nombres.Take(contador).ToArray();
+        }
+
+        public string[] GetApellidos()
+        {
+
+        }
+    }
+}
